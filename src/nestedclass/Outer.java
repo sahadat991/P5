80531d19f81d0716ee940ed0ecc76590b1e171fd
sayem <sayem991@gmail.com>
2017-08-06 16:59:48 -0400

    frist time

--- src/nestedclass/Outer.java
+++ src/nestedclass/Outer.java
@@ -1,5 +1,31 @@
 package nestedclass;
 
 public class Outer {
-
+	
+	Food inner1 = new Food();
+	Water inner2 = new Water();
+	Education inner3 = new Education();
+	
+	public void outterM() {
+		System.out.println("Human basic needs");
+		inner1.inner();	
+		inner2.inner();
+		inner3.inner();
+	}
+	private class Food{
+		private void inner(){
+			System.out.println("");
+		}
+	}		
+		private class Water{
+			private void inner(){
+				System.out.println("This is Inner B");	
+		}
+	}
+	
+		private class Education{
+			private void inner(){
+				System.out.println("This is Inner C");
+			}
+		}
 }
--- src/nestedclass/TestOuter.java
+++ src/nestedclass/TestOuter.java
@@ -2,4 +2,9 @@ package nestedclass;
 
 public class TestOuter {
 
+	Outer Methods = new Outer();
+	Methods.
+	
+}
+
 }
